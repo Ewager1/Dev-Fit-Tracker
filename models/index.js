b7fcfb8598fb57b65494d0e41b5b78a38a932032
EWager <ethanbwager@gmail.com>
2021-02-06 16:44:32 -0500

    set up server and succesfully added seed data

---
+++ models/index.js
@@ -0,0 +1,2 @@
+const Workout = require("./models.js")
+module.exports = { Workout }
---
+++ models/models.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const WorkoutSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    required: "workout type required. ex. resistance, cardio",
+  },
+
+  name: {
+    type: String,
+    trim: true,
+    required: "workout name required ex. bicept curl",
+  },
+
+  duration: {
+    type: Number,
+    required: "Please enter how long you exercised for",
+  },
+
+  weight: {
+    type: Number,
+  },
+
+  reps: {
+    type: Number,
+  },
+
+  sets: {
+    type: Number,
+  },
+
+  distance: {
+    type: Number,
+  },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+
+
+module.exports = Workout;
