dc4955ebb889948c97e20d5618b72e7582818383
EWager <ethanbwager@gmail.com>
2021-02-09 13:52:49 -0500

    added date to model so all workouts have proper date

--- models/models.js
+++ models/models.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+  day: {
+    type: Date, 
+    default: Date.now()
+  },
+  
   exercises: [
     {
       type: {
